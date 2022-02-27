8884f54c5c82145ef17e2fc14d6ebf1392cfbecd
Tina <migujeong@gmail.com>
2022-02-27 23:29:09 +0900

    modify introduction and add new post

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,7 +1,7 @@
 const projectsData = [
   {
     title: 'Mydata Plugin',
-    description: `금융정보를 한 눈에 모아보고, 지출내역을 분석해주는 어플리케이션입니다.`,
+    description: `나의 금융정보를 한 눈에 모아보고, 지출내역을 분석해주는 어플리케이션입니다.`,
     imgSrc: '/static/images/mydata_plugin.png',
     href: '',
   },

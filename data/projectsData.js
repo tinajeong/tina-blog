76f061f8682e316081a375d3f671ec397146128d
Tina <migujeong@gmail.com>
2023-05-17 15:34:07 +0900

    add projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -5,6 +5,12 @@ const projectsData = [
     imgSrc: '/static/images/mydata_plugin.png',
     href: '',
   },
+  {
+    title: '다독다독',
+    description: `책에 대한 인사이트를 공유하고 소통하는 독서 소셜 플랫폼입니다.`,
+    imgSrc: '/static/images/dadok.png',
+    href: 'https://dev.dadok.site',
+  },
 ]
 
 export default projectsData
